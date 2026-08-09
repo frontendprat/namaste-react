89d010a1f5e7730c6a1b582cb4d4dd325e1ec428
vyas10 <pratikvyas9875@gmail.com>
2026-08-09 21:16:32 +0530

    lecture updated

--- App.js
+++ App.js
@@ -1,28 +1,44 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-/**
- * <div id="parent">
- *  <div id="child">
- *      <h1>I'm h1 tag</h1>
- *      <h2>I'm h2 tag</h2>
- *   </div>
- * </div>
- */
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
-  React.createElement("div", { id: "child" }),
-  [
-    React.createElement("h1", { id: "headingChild" }, "I'm h1 tag"),
-    React.createElement("h2", { id: "headingChild2" }, "I'm h2 tag"),
-  ], // Array of children (sibling)
-);
-const heading = React.createElement(
-  "h1",
-  { id: "heading" },
-  "Hello world from React",
-);
 
-console.log(heading); // object
+// const heading = React.createElement(
+//   "h1",
+//   { id: "heading" },
+//   "Namaste React 🚀",
+// );
+// JSX
+
+// JSX is transpiled before it reaches the JS - PARCEL - Babel
+
+// React Element
+const jsxHeading = <h1 className="head">Namaste React 🚀 using JSX</h1>;
+
+// React Components
+// Class based componets - old
+
+// Functional components - new (just a js function which returns some jsx)
+const Title = () => {
+  return (
+    <div className="container">
+      <h1> This is a title component</h1>
+    </div>
+  );
+};
+
+const elem = <h2>This is elem</h2>;
+
+const HeadingComponent = () => {
+  return (
+    <div className="container">
+      <Title />
+      {number}
+      {elem}
+      <h1> This is a functional React component</h1>
+    </div>
+  );
+};
+
+const number = 10000;
+console.log(jsxHeading);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent); // converts the object in to the html needed
+root.render(<HeadingComponent />);
