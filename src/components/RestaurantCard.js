a0b8d32779fb0f3960dbb901261788e2daba6b0b
vyas10 <pratikvyas9875@gmail.com>
2026-08-22 17:38:55 +0530

    lecture updated and cors issue resolved

--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -8,21 +8,25 @@ const RestaurantCard = (props) => {
     cuisines,
     costForTwo,
     deliveryTime,
-  } = resData?.data;
+  } = resData?.info;
 
   return (
     <div className="res-card" style={{ backgroundColor: "#f0f0f0" }}>
-      {console.log(cloudinaryImageId)}
       <img
         className="res-logo"
         alt="res-logo"
-        src={+cloudinaryImageId + ".jpg"}
+        src={
+          "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/" +
+          cloudinaryImageId
+        }
       />
       <h3>{name}</h3>
       <h4>{cuisines.join(", ")}</h4>
-      <h4>{avgRating} stars</h4>
-      <h4>₹{costForTwo / 100} FOR TWO</h4>
-      <h4>{deliveryTime} minutes</h4>
+      <div className="flex-center">
+        <h4>{avgRating} stars</h4>
+        <h4>{costForTwo}</h4>
+      </div>
+      <h4>{resData?.info.sla?.deliveryTime} minutes</h4>
     </div>
   );
 };
